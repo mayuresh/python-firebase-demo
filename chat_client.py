8060a17894a39533a47105b008afd3388874d307
mayuresh <mayuresh@gmail.com>
2015-02-16 19:28:03 +0530

    Changed indentation. Changed function names.

--- chat_client.py
+++ chat_client.py
@@ -1,37 +1,39 @@
 from multiprocessing import Process
-from firebase import  firebase
+from firebase import firebase
 from sseclient import SSEClient
-import time
-import json
 
-roomId = "Room000001"
-roomPath = "/Rooms/" + roomId 
-tweetsPath = roomPath + "/Tweets"
+FIREBASE_URL = "https://dazzling-fire-5952.firebaseio.com/"
 
-def poll_target():
-	sse = SSEClient("https://dazzling-fire-5952.firebaseio.com/PythonChatDemo/Track.json")
-	print("Watching Firebase node - %s" % ("https://dazzling-fire-5952.firebaseio.com/PythonChatDemo/Track.json"))
-	for t in sse:
-		t_data = json.loads(t.data)	
-		if t_data is None: # Keep alive
-			continue
 
-		if t_data["data"] is None:
-			continue
-		
-		for (k, v) in t_data["data"].items():
-			print("%s says: %s" % (k, v))
+def poll_chat():
+    sse = SSEClient(FIREBASE_URL + "PythonChatDemo/Track.json")
+    print("Watching Firebase node - %s" % (FIREBASE_URL + "PythonChatDemo/Track.json"))
+    for t in sse:
+        t_data = json.loads(t.data)
+        if t_data is None:  # Keep alive
+            continue
 
-#Main
+        if t_data["data"] is None:
+            continue
+
+        for (k, v) in t_data["data"].items():
+            print("%s says: %s" % (k, v))
+
+# Main
 if __name__ == '__main__':
 
-	username = input("Input your name: ")
-	fb = firebase.FirebaseApplication("https://dazzling-fire-5952.firebaseio.com/", None)
+    # Start a thread to monitor changes to firebase
+    t = Process(target=poll_chat)
+    t.start()
 
-	fb.post('/PythonChatDemo/Track', {username: "%s joined the chat" % (username)})
-	t = Process(target=poll_target)
-	t.start()
+    username = input("Input your name: ")
+    fb = firebase.FirebaseApplication(FIREBASE_URL, None)
 
-	while(True):
-		message = input("")
-		fb.post('/PythonChatDemo/Track', {username: message})
+    # Post initial message to Firebase
+    fb.post('/PythonChatDemo/Track', {username: "%s joined the chat" % (username)})
+
+
+    # Post new messages to Firebase
+    while (True):
+        message = input("")
+        fb.post('/PythonChatDemo/Track', {username: message})
