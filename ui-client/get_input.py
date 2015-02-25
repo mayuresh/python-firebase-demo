ddfacd0a674a3668c7590053d0ad10ce7ba3fb1b
mayuresh <mayuresh@gmail.com>
2015-02-25 17:09:35 +0530

    removed unnecessary files

--- ui-client/get_input.py
+++
@@ -1,9 +0,0 @@
-from firebase import firebase
-firebase = firebase.FirebaseApplication("https://dazzling-fire-5952.firebaseio.com/", None)
-
-while(True):
-	key = input("Input a key: ")
-	value = input("Input a value: ")
-
-	firebase.post('/PythonDemo/Track', {key: value})
-	print("%s added to Firebase at %s\n" % ({key: value}, "https://dazzling-fire-5952.firebaseio.com/" + "/PythonDemo/Track"))
